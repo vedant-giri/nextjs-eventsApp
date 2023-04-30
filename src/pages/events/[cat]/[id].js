118510128d47ffbce5ff56fdd554ae22afa113c0
Vedant Giri <113013212+vedant-giri@users.noreply.github.com>
2023-04-30 10:42:00 +0530

    changes made

--- src/pages/events/[cat]/[id].js
+++ src/pages/events/[cat]/[id].js
@@ -29,7 +29,7 @@ export async function getStaticPaths(){
 export async function getStaticProps(context){
   
   const id= context.params.id;
-  const {allEvents} =await import('/data/data.json');
+  const {allEvents} =await import('/tmp-/data.json');
   const eventData=allEvents.find((ev)=>id ===ev.id);
   return{
     props:{data:eventData},
